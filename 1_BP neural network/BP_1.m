5ea972564f250a5be4026808e25347a491b6a6d5
lizi1234560 <1873209744@qq.com>
2019-09-25 20:12:55 +0800

    Update BP_1.m

--- 1_BP neural network/BP_1.m
+++ 1_BP neural network/BP_1.m
@@ -4,13 +4,11 @@ LMCI = ones(1, 26);
 NC = ones(1, 36) + 1;
 labels = [LMCI, NC];
 
-
 n = floor(0.6 * 62);
 x = randperm(62);
 y = x(1:n);       
 
 z = x(n+1:62);    
-
 
 test_num = length(z);      
 sam_num = 36;             
@@ -21,8 +19,7 @@ rate = zeros(1,N);
 fc = zeros(N, fc_num);
 predictY = zeros(N, test_num);
 
-for i = 1:N
-    
+for i = 1:N    
    
     sample_num = randsample(length(y), sam_num, 'true');
     trainsam(i, :) = y(sample_num);
@@ -30,13 +27,10 @@ for i = 1:N
     trainl = labels(sample_num);
   
     FC(trainsam(i, :),:);        
-    
-    
-  
+
     temp = find(Chrom(i, :) == 1);
     fc(i, :) = randsample(length(temp), fc_num, 'true');
-  
-   
+
     fc_temp = fc(i, :);
     
     traind = FC(trainsam(i, :), fc_temp);  
@@ -44,24 +38,20 @@ for i = 1:N
     
     testd = FC(z ,fc_temp);   
     testl = labels(z);
-    
-  
+
     net = feedforwardnet(5);	
-    
- 
+
     net.trainFcn='trainbfg';       
     net.trainparam.epochs=500;       
     net.trainParam.showCommandLine = 0;
     net.trainParam.showWindow = 0;
     net.trainparam.goal=0.01;
     net=train(net,traind',trainl);
-    
 
     test_out = sim(net,testd');
     predictY(i, :) = vec2ind(test_out);
     rate(i) = sum(predictY(i, :) == testl) / length(testl);
 end
-
 
 Res_sum = sum(predictY);
 Result = sign(Res_sum);
